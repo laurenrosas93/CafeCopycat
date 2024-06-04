7d2ebb4a13de000ef7c60134bef156991b377f74
laurenrosas93 <95515803+laurenrosas93@users.noreply.github.com>
2024-06-04 16:01:14 -0400

    1st commit
    github HW first change

--- hello.py
+++ hello.py
@@ -1,1 +1,5 @@
 print("hello")
+
+# hello-world ( from hello world project)
+This repo is for practicing the GitHub Flow.
+Hi! My Name is Lauren Rosas. I am an HCI graduate student and expected to graduate in Fall 2024.
